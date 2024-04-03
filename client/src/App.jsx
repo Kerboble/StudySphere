5214ff2ffe330a5c742a39e37c42aa80ca8b8613
Kerboble <brennen.bogueca2023@gmail.com>
2024-04-03 16:43:55 -0700

    users can register and log in

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+// App.js
+import React from 'react';
+import Registration from './Pages/Registration';
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/">
+        <Route index element={<Registration />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path='/home' element={<Home />}/>
+    </Route>
+   </Routes>
+   </BrowserRouter>
+  );
+};
+
+export default App;
