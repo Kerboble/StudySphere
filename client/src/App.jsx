92f8f4004140f15bfd50d417832aefd4c3494f56
Kerboble <brennen.bogueca2023@gmail.com>
2024-04-19 12:52:22 -0700

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import { AuthContext } from './context/authContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AdminStudents from './components/AdminStudents';
-import AdminDashboard from './components/adminDashboard';
+import AdminDashboard from './components/AdminDashboard';
 import AdminCohorts from './components/AdminCohorts';
 import AdminTeachers from './components/AdminTeachers';
 
--- client/src/components/AdminCohorts.jsx
+++ client/src/components/AdminCohorts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AdminCohorts() {
   return (
-    <div>AdminCohorts</div>
+    <div>test</div>
   )
 }
 
